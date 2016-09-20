759021d735e14a29b939942dea861c2d45536b36
Dave Renzo III <dave.p.renzoiii@rrd.com>
2016-09-20 10:32:44 -0400

    initial timer experimens

---
+++ TinyLFO/TinyLFO/main.c
@@ -0,0 +1,38 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+volatile uint16_t phase_accumulator =0;
+
+int main(void)
+{
+	//setup output led
+	DDRA =0xff;
+	PORTA |= (1<<PORTA0);
+	PORTA |= (1<<PORTA1);
+	PORTA |= (1<<PORTA2);
+	PORTA |= (1<<PORTA3);
+	PORTA |= (1<<PORTA4);
+	PORTA |= (1<<PORTA5);
+	PORTA |= (1<<PORTA6);
+	PORTA |= (1<<PORTA7);
+
+
+	//enable timer interrupt
+	TIMSK0 |= (1 << TOIE0);
+	TCCR0B = (1<< CS01)|(1<< CS00);
+
+	//enable interrupts
+	sei(); 
+    
+    while (1) 
+    {
+    }
+}
+
+//isr
+SIGNAL (TIM0_OVF_vect)
+{
+	phase_accumulator+=0xfc0;
+	PORTA = phase_accumulator >>8 ;
+	
+}
